ba66949c6a3fcd6a08c49ba7a0854d3bc402a878
mydearxym <mydearxym@qq.com>
2019-10-26 11:36:47 +0800

    fix(markdown): refactor code, add more inline docs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import './index.css'
+import "./index.css";
 
-import { 
-  checkMarkdownSyntax, 
-  checkInlineMarkdownSyntax, 
-  MD_TYPE, 
+import {
+  checkMarkdownSyntax,
+  checkInlineMarkdownSyntax,
+  MD_TYPE,
   ANCHOR,
   insertHtmlAtCaret,
   selectNode,
   markdownBlockConfig
-} from './utils'
+} from "./utils";
 
 /**
  * Base Paragraph Block for the Editor.js.
@@ -33,7 +33,7 @@ export default class Paragraph {
    * @constructor
    */
   static get DEFAULT_PLACEHOLDER() {
-    return '';
+    return "";
   }
 
   /**
@@ -44,13 +44,13 @@ export default class Paragraph {
    *   config - user config for Tool
    *   api - Editor.js API
    */
-  constructor({data, config, api}) {
+  constructor({ data, config, api }) {
     this.api = api;
 
     this._CSS = {
       block: this.api.styles.block,
-      wrapper: 'ce-paragraph',
-      mention: 'cdx-mention'
+      wrapper: "ce-paragraph",
+      mention: "cdx-mention"
     };
     this.onKeyUp = this.onKeyUp.bind(this);
 
@@ -58,14 +58,14 @@ export default class Paragraph {
      * Placeholder for paragraph if it is first Block
      * @type {string}
      */
-    this._placeholder = config.placeholder ? config.placeholder : Paragraph.DEFAULT_PLACEHOLDER;
+    this._placeholder = config.placeholder
+      ? config.placeholder
+      : Paragraph.DEFAULT_PLACEHOLDER;
     this._data = {};
     this._element = this.drawView();
 
     this.data = data;
   }
-
-  
 
   /**
    * Check if text content is empty and set empty string to inner html.
@@ -74,17 +74,17 @@ export default class Paragraph {
    * @param {KeyboardEvent} e - key up event
    */
   onKeyUp(e) {
-    if (e.code !== 'Backspace' && e.code !== 'Delete') {
+    if (e.code !== "Backspace" && e.code !== "Delete") {
       return;
     }
 
-    console.log('delete detect');
+    console.log("delete detect");
     this.deleteTagsIfNeed();
 
-    const {textContent} = this._element;
+    const { textContent } = this._element;
 
-    if (textContent === '') {
-      this._element.innerHTML = '';
+    if (textContent === "") {
+      this._element.innerHTML = "";
     }
   }
 
@@ -95,34 +95,39 @@ export default class Paragraph {
    * delete tags like mention, ..
    * @return {void}
    * @private
-  */
+   */
   deleteTagsIfNeed() {
     if (window.getSelection) {
       let sel = window.getSelection();
-      // console.log("sel: ", sel)
-      // console.log("sel.anchorNode.parentNode.nodeName: ", sel.anchorNode.parentNode.nodeName)
-      // console.log("sel.anchorNode.parentNode.className", sel.anchorNode.parentNode.className)
-      // console.log("sel.anchorNode.parentNode.remove: ", sel.anchorNode.parentNode.remove)
 
-      if(sel.anchorNode.parentNode.className === this._CSS.mention) {
+      if (sel.anchorNode.parentNode.className === this._CSS.mention) {
         sel.anchorNode.parentNode.remove();
       }
+    } else {
+      console.log(
+        "editor paragraph plugin, window dfgetSelection is not supported."
+      );
     }
   }
 
+  /**
+   * handle inline markdown syntax like bold, italic, inline-code etc..
+   * @return {HTMLDivElement}
+   * @private
+   */
   handleInlineMDShortcut(ev) {
     const curBlockIndex = this.api.blocks.getCurrentBlockIndex();
     const curBlock = this.api.blocks.getBlockByIndex(curBlockIndex);
-  
-    const { isValid, md, html } = checkInlineMarkdownSyntax(curBlock, ev.data)
+
+    const { isValid, md, html } = checkInlineMarkdownSyntax(curBlock, ev.data);
     if (isValid) {
-      const INLINE_MD_HOLDER = `<span id="${ANCHOR.INLINE_MD}" />`
+      const INLINE_MD_HOLDER = `<span id="${ANCHOR.INLINE_MD}" />`;
 
       // 改变 innerHTML 以后光标会到内容的最开始，需要埋一个点，完事后在选中
-      insertHtmlAtCaret(INLINE_MD_HOLDER)
-      ev.target.innerHTML = ev.target.innerHTML.replace(md, html)
+      insertHtmlAtCaret(INLINE_MD_HOLDER);
+      ev.target.innerHTML = ev.target.innerHTML.replace(md, html);
       selectNode(document.querySelector(`#${ANCHOR.INLINE_MD}`));
-      document.querySelector(`#${ANCHOR.INLINE_MD}`).remove()
+      document.querySelector(`#${ANCHOR.INLINE_MD}`).remove();
 
       // 防止插入粗体以后以后输入一直是粗体。。
       insertHtmlAtCaret(ANCHOR.SPACE);
@@ -132,26 +137,26 @@ export default class Paragraph {
   // handle markdown shortcuts
   /**
    * handle markdown shortcuts
-   * currently support header, list
+   * currently support header, list, code, etc...
    * @param ev {HTMLElementEvent}
    * @return {void}
    * @private
-  */
+   */
   handleMDShortcut(ev) {
     const curBlockIndex = this.api.blocks.getCurrentBlockIndex();
     const curBlock = this.api.blocks.getBlockByIndex(curBlockIndex);
 
-    const { isValidMDStatus, MDType } = checkMarkdownSyntax(curBlock, ev.data)
-    if(!isValidMDStatus) return false;
+    const { isValidMDStatus, MDType } = checkMarkdownSyntax(curBlock, ev.data);
+    if (!isValidMDStatus) return false;
 
     // delete current block
-    const { isInvalid, type, toolData, config } = markdownBlockConfig(MDType)
+    const { isInvalid, type, toolData, config } = markdownBlockConfig(MDType);
 
-    if(!isInvalid) {
+    if (!isInvalid) {
       this.api.blocks.delete(curBlockIndex);
       this.api.blocks.insert(type, toolData, config, curBlockIndex);
-          // set cursor to first char
-      this.api.caret.setToBlock(curBlockIndex, 'start');
+      // set cursor to first char
+      this.api.caret.setToBlock(curBlockIndex, "start");
     }
   }
 
@@ -160,11 +165,15 @@ export default class Paragraph {
    * @param ev {HTMLElementEvent}
    * @return {HTMLElementEvent}
    * @private
-  */
+   */
   handleMention(ev) {
-    if(ev.data === '@') {
-      const mentionClass = this._CSS.mention
-      const mention = `<span class="${mentionClass}" contenteditable="false" id="${mentionClass}" tabindex="1" style="opacity: 1;">.</span>`;
+    if (ev.data === "@") {
+      const mentionClass = this._CSS.mention;
+      const mention = `<span class="${
+        mentionClass
+      }" contenteditable="false" id="${
+        mentionClass
+      }" tabindex="1" style="opacity: 1;">.</span>`;
       const mentionId = `#${mentionClass}`;
 
       insertHtmlAtCaret(mention);
@@ -172,9 +181,12 @@ export default class Paragraph {
       insertHtmlAtCaret(ANCHOR.SPACE);
       insertHtmlAtCaret(ANCHOR.SPACE);
 
-      const mentionParent = document.querySelector(mentionId).parentElement
-      console.log('mentionParent ', mentionParent.innerHTML )
-      mentionParent.innerHTML = mentionParent.innerHTML.replace('@' + mention, mention)
+      const mentionParent = document.querySelector(mentionId).parentElement;
+      console.log("mentionParent ", mentionParent.innerHTML);
+      mentionParent.innerHTML = mentionParent.innerHTML.replace(
+        "@" + mention,
+        mention
+      );
       selectNode(document.querySelector(mentionId));
     }
   }
@@ -185,13 +197,13 @@ export default class Paragraph {
    * @private
    */
   drawView() {
-    let div = document.createElement('DIV');
+    let div = document.createElement("DIV");
 
     div.classList.add(this._CSS.wrapper, this._CSS.block);
     div.contentEditable = true;
     div.dataset.placeholder = this._placeholder;
 
-    div.addEventListener('keyup', this.onKeyUp);
+    div.addEventListener("keyup", this.onKeyUp);
 
     // const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
     // let observer = new MutationObserver(myFunction);
@@ -199,12 +211,17 @@ export default class Paragraph {
     //   childList: true
     // });
 
-    this.api.listeners.on(div, 'input', (ev) => {
-      this.handleMDShortcut(ev);
-      this.handleInlineMDShortcut(ev);
-      this.handleMention(ev);
-      // console.log('Button clicked!');
-   }, true);
+    this.api.listeners.on(
+      div,
+      "input",
+      ev => {
+        this.handleMDShortcut(ev);
+        this.handleInlineMDShortcut(ev);
+        this.handleMention(ev);
+        // console.log('Button clicked!');
+      },
+      true
+    );
 
     // div.addEventListener('input', (ev) => {
     //   this.handleMDShortcut(ev);
@@ -232,7 +249,7 @@ export default class Paragraph {
    */
   merge(data) {
     let newData = {
-      text : this.data.text + data.text
+      text: this.data.text + data.text
     };
 
     this.data = newData;
@@ -247,7 +264,7 @@ export default class Paragraph {
    * @public
    */
   validate(savedData) {
-    if (savedData.text.trim() === '') {
+    if (savedData.text.trim() === "") {
       return false;
     }
 
@@ -284,8 +301,8 @@ export default class Paragraph {
    */
   static get conversionConfig() {
     return {
-      export: 'text', // to convert Paragraph to other block, use 'text' property of saved data
-      import: 'text' // to covert other block's exported string to Paragraph, fill 'text' property of tool data
+      export: "text", // to convert Paragraph to other block, use 'text' property of saved data
+      import: "text" // to covert other block's exported string to Paragraph, fill 'text' property of tool data
     };
   }
 
@@ -295,7 +312,7 @@ export default class Paragraph {
   static get sanitize() {
     return {
       text: {
-        br: true,
+        br: true
       }
     };
   }
@@ -324,7 +341,7 @@ export default class Paragraph {
   set data(data) {
     this._data = data || {};
 
-    this._element.innerHTML = this._data.text || '';
+    this._element.innerHTML = this._data.text || "";
   }
 
   /**
@@ -335,7 +352,7 @@ export default class Paragraph {
    */
   static get pasteConfig() {
     return {
-      tags: [ 'P' ]
+      tags: ["P"]
     };
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,30 @@
+/**
+ * some hidden dom anchors for editor.js hack 
+ * 
+ */
 export const ANCHOR = {
-  'SPACE': '&nbsp;',
-  'INLINE_MD': 'inline_tmp_anchor',
-}
+  SPACE: "&nbsp;",
+  INLINE_MD: "inline_tmp_anchor"
+};
 
+/**
+ * supported markdown syntax constants 
+ * NOTE:  inline markdown syntax is not included, like bold/italic/inline-code etc..
+ */
 export const MD_TYPE = {
-  "HEADER_1": "HEADER_1",
-  "HEADER_2": "HEADER_2",
-  "HEADER_3": "HEADER_3",
-  "UNORDERED_LIST": "UNORDERED_LIST",
-  "ORDERED_LIST": "ORDERED_LIST",
-  "QUOTE": "QUOTE",
-  "CODE": "CODE",
-}
+  HEADER_1: "HEADER_1",
+  HEADER_2: "HEADER_2",
+  HEADER_3: "HEADER_3",
+  UNORDERED_LIST: "UNORDERED_LIST",
+  ORDERED_LIST: "ORDERED_LIST",
+  QUOTE: "QUOTE",
+  CODE: "CODE"
+};
 
-// see https://www.markdownguide.org/basic-syntax/
+/**
+ * Base RegExp to detect inline markdown syntax
+ * see https://www.markdownguide.org/basic-syntax/
+ */
 const MD_REG = {
   // BOLD_old: new RegExp(/\*\*(.*?)\*\*/),
   BOLD: new RegExp(/\*\*([\S]{1,})\*\*/),
@@ -21,177 +32,167 @@ const MD_REG = {
   // NOTE:  marker is extended
   MARKER: new RegExp(/==([\S]{1,})==/),
   INLINECODE: new RegExp(/\`([\S]{1,})\`/)
-}
+};
 
 /**
- * TODO
+ * check main markdown syntax like header, code, list, quote etc
+ * supported syntax are list in MD_TYPE
  *
- * @param {{data: ParagraphData, config: object, api: object}}
- *   data — previously saved data
- *   config - user config for Tool
- *   api - Editor.js API
+ * @param curBlock {object} editor.js's edit block
+ * @param data {string} current input data, data === ' ' means input something followed by a space
+ *        which is used to trigger the markdown block
  */
 export const checkMarkdownSyntax = function(curBlock, data) {
-  const blockText = curBlock.textContent.trim()
-  let isValidMDStatus = true
-  let MDType = ""
+  const blockText = curBlock.textContent.trim();
+  let isValidMDStatus = true;
+  let MDType = "";
+  const isFollowedBySpace = data === " " ? true : false;
 
-  // TODO:  length check if blockText.length > 6 break
+  // if input length is larger then longest markdown sytax(###### ), is no need to check
+  if (blockText.length > 7) return { isValidMDStatus: false, MDType };
 
-  switch(true) {
-    case blockText === '#' && data === ' ': {
-      MDType = MD_TYPE.HEADER_1
-      break
+  switch (true) {
+    case blockText === "#" && isFollowedBySpace: {
+      MDType = MD_TYPE.HEADER_1;
+      break;
     }
-    case blockText === '##' && data === ' ': {
-      MDType = MD_TYPE.HEADER_2
-      break
-    }
-
-    case blockText === '###' && data === ' ': {
-      MDType = MD_TYPE.HEADER_3
-      break
+    case blockText === "##" && isFollowedBySpace: {
+      MDType = MD_TYPE.HEADER_2;
+      break;
     }
 
-    case blockText === '####' && data === ' ': {
-      MDType = MD_TYPE.HEADER_3
-      break
+    case (blockText === "###" ||
+      blockText === "####" ||
+      blockText === "#####" ||
+      blockText === "######") &&
+      isFollowedBySpace: {
+      MDType = MD_TYPE.HEADER_3;
+      break;
     }
 
-    case blockText === '#####' && data === ' ': {
-      MDType = MD_TYPE.HEADER_3
-      break
+    case blockText === "-" && isFollowedBySpace: {
+      MDType = MD_TYPE.UNORDERED_LIST;
+      break;
     }
 
-    case blockText === '######' && data === ' ': {
-      MDType = MD_TYPE.HEADER_3
-      break
+    case blockText === "1" && isFollowedBySpace: {
+      MDType = MD_TYPE.ORDERED_LIST;
+      break;
     }
 
-    case blockText === '-' && data === ' ': {
-      MDType = MD_TYPE.UNORDERED_LIST
-      break
+    case blockText === ">" && isFollowedBySpace: {
+      MDType = MD_TYPE.QUOTE;
+      break;
     }
 
-    case blockText === '1' && data === ' ': {
-      MDType = MD_TYPE.ORDERED_LIST
-      break
-    }
-    
-    case blockText === '>' && data === ' ': {
-      MDType = MD_TYPE.QUOTE
-      break
-    }
-
-    case blockText === '```': {
-      MDType = MD_TYPE.CODE
-      break
+    case blockText === "```": {
+      MDType = MD_TYPE.CODE;
+      break;
     }
 
     default: {
-      isValidMDStatus = false
+      isValidMDStatus = false;
     }
   }
 
-  return { isValidMDStatus, MDType }
-}
+  return { isValidMDStatus, MDType };
+};
 
 export const markdownBlockConfig = function(type) {
-  switch(type) {
-    case MD_TYPE.HEADER_1: 
+  switch (type) {
+    case MD_TYPE.HEADER_1:
       return {
-        type: 'header',
-        toolData: {'level': 1},
-        config: {},
-      }
-    case MD_TYPE.HEADER_2: 
+        type: "header",
+        toolData: { level: 1 },
+        config: {}
+      };
+    case MD_TYPE.HEADER_2:
       return {
-        type: 'header',
-        toolData: {'level': 2},
-        config: {},
-      }
-    case MD_TYPE.HEADER_3: 
+        type: "header",
+        toolData: { level: 2 },
+        config: {}
+      };
+    case MD_TYPE.HEADER_3:
       return {
-        type: 'header',
-        toolData: {'level': 3},
-        config: {},
-      }
-    
-    case MD_TYPE.UNORDERED_LIST: 
-      return {
-        type: 'list',
-        toolData: {'style': 'unordered'},
-        config: {},
-      }
+        type: "header",
+        toolData: { level: 3 },
+        config: {}
+      };
 
-    case MD_TYPE.ORDERED_LIST: 
+    case MD_TYPE.UNORDERED_LIST:
       return {
-        type: 'list',
-        toolData: {'style': 'ordered'},
-        config: {},
-      }
-    
-    case MD_TYPE.QUOTE: 
-      return {
-        type: 'quote',
-        toolData: {},
-        config: {},
-      }
+        type: "list",
+        toolData: { style: "unordered" },
+        config: {}
+      };
 
-    case MD_TYPE.CODE: 
+    case MD_TYPE.ORDERED_LIST:
       return {
-        type: 'code',
+        type: "list",
+        toolData: { style: "ordered" },
+        config: {}
+      };
+
+    case MD_TYPE.QUOTE:
+      return {
+        type: "quote",
         toolData: {},
-        config: {},
-      }
+        config: {}
+      };
+
+    case MD_TYPE.CODE:
+      return {
+        type: "code",
+        toolData: {},
+        config: {}
+      };
 
     default: {
-      return { isInvalid: false, type: '', toolData: '', config: '' }
+      return { isInvalid: false, type: "", toolData: "", config: "" };
     }
   }
-}
+};
 
 // parse the match info given by string.match(regex)
 const parseMatchTexts = function(texts) {
   return {
     md: texts[0],
     content: texts[1],
-    isValid:  true, // texts[1].length > 0
-  }
-}
+    isValid: true // texts[1].length > 0
+  };
+};
 
 // inline markdown syntax
 export const checkInlineMarkdownSyntax = function(curBlock, data) {
-  const blockText = curBlock.textContent.trim()
-  const { BOLD, ITALIC, MARKER, INLINECODE } = MD_REG
+  const blockText = curBlock.textContent.trim();
+  const { BOLD, ITALIC, MARKER, INLINECODE } = MD_REG;
 
-  const boldTexts = blockText.match(BOLD)
-  if(boldTexts) {
-    const { isValid, md, content } = parseMatchTexts(boldTexts)
-    return { isValid, md, html: `<b>${content}</b>` }
+  const boldTexts = blockText.match(BOLD);
+  if (boldTexts) {
+    const { isValid, md, content } = parseMatchTexts(boldTexts);
+    return { isValid, md, html: `<b>${content}</b>` };
   }
 
-  const markerTexts = blockText.match(MARKER)
-  if(markerTexts) {
-    const { isValid, md, content } = parseMatchTexts(markerTexts)
-    return { isValid, md, html: `<mark class="cdx-marker">${content}</mark>` }
+  const markerTexts = blockText.match(MARKER);
+  if (markerTexts) {
+    const { isValid, md, content } = parseMatchTexts(markerTexts);
+    return { isValid, md, html: `<mark class="cdx-marker">${content}</mark>` };
   }
 
-  const italicTexts = blockText.match(ITALIC)
-  if(italicTexts) {
-    const { isValid, md, content } = parseMatchTexts(italicTexts)
-    return { isValid, md, html: `<i>${content}</i>` }
+  const italicTexts = blockText.match(ITALIC);
+  if (italicTexts) {
+    const { isValid, md, content } = parseMatchTexts(italicTexts);
+    return { isValid, md, html: `<i>${content}</i>` };
   }
 
-  const inlineCodeTexts = blockText.match(INLINECODE)
-  if(inlineCodeTexts) {
-    const { isValid, md, content } = parseMatchTexts(inlineCodeTexts)
-    return { isValid, md, html: `<code class="inline-code">${content}</code>` }
+  const inlineCodeTexts = blockText.match(INLINECODE);
+  if (inlineCodeTexts) {
+    const { isValid, md, content } = parseMatchTexts(inlineCodeTexts);
+    return { isValid, md, html: `<code class="inline-code">${content}</code>` };
   }
 
-  return { isValid: false, text: ''}
-}
-
+  return { isValid: false, text: "" };
+};
 
 // NOTE:  html is string
 // see: https://stackoverflow.com/questions/6690752/insert-html-at-caret-in-a-contenteditable-div
@@ -208,12 +209,14 @@ export const insertHtmlAtCaret = function(html) {
 
       // Range.createContextualFragment() would be useful here but is
       // non-standard and not supported in all browsers (IE9, for one)
-      const el = document.createElement('div');
+      const el = document.createElement("div");
 
       el.innerHTML = html;
-      var frag = document.createDocumentFragment(), node, lastNode;
+      var frag = document.createDocumentFragment(),
+        node,
+        lastNode;
 
-      while ( (node = el.firstChild) ) {
+      while ((node = el.firstChild)) {
         lastNode = frag.appendChild(node);
       }
       range.insertNode(frag);
@@ -228,7 +231,7 @@ export const insertHtmlAtCaret = function(html) {
       }
     }
   }
-}
+};
 
 // select a html node
 export const selectNode = function(node) {
@@ -253,8 +256,6 @@ export const selectNode = function(node) {
     selection.addRange(range);
     // console.log('2 -->', range.extractContents());
   } else {
-    console.warn('Could not select text in node: Unsupported browser.');
+    console.warn("Could not select text in node: Unsupported browser.");
   }
-}
-
-
+};
